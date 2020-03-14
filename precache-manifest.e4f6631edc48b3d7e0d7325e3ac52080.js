17d940e67f190171ee50d3bb5ea6c26363368246
Cesar Arroyo <cesardavid_09@hotmail.com>
2020-03-14 16:13:20 -0500

    Updates

---
+++ precache-manifest.e4f6631edc48b3d7e0d7325e3ac52080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4818afbe6e37863260e5022e6b70d8",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "314cfcf84cd7f809ffa7",
+    "url": "/myPortfolio/static/css/main.419370d8.chunk.css"
+  },
+  {
+    "revision": "7c896e4508edd18f1f34",
+    "url": "/myPortfolio/static/js/2.f1f0098a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myPortfolio/static/js/2.f1f0098a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "314cfcf84cd7f809ffa7",
+    "url": "/myPortfolio/static/js/main.ce6c816a.chunk.js"
+  },
+  {
+    "revision": "618bcadaf35e609ccee4",
+    "url": "/myPortfolio/static/js/runtime-main.6e8f8731.js"
+  }
+]);
